1840b7aa8e8e36d51c87e45f9a5a8d360d187275
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-01-13 17:43:26 +0800

    Forgot to actually export the components

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-import { TestComponent } from "./components/TestComponent/TestComponent";
-import { TxtCharsFadeIn } from "./components/TextCharacterFadeIn/TxtCharsFadeIn";
+import {TestComponent} from "./components/TestComponent/TestComponent";
+import {TxtCharsFadeIn} from "./components/TextCharacterFadeIn/TxtCharsFadeIn";
+import {DnDList} from "./components/DnDList/DnDList";
+import {DnDItemContainer, DnDItem} from "./components/DragAndDrop/DragAndDrop";
 import "./index.css"
+
 export {
     TestComponent,
-    TxtCharsFadeIn
+    TxtCharsFadeIn,
+    DnDList,
+    DnDItemContainer,
+    DnDItem
 }
